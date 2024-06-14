94c19fb4215b6a4095b63d53f7f635cdee385a1a
Vittoria <vittoriaalataazzurra@gmail.com>
2024-06-14 18:12:56 +0200

    MSI & CERT

--- dl.js
+++ dl.js
@@ -1,6 +1,6 @@
 function downloadFiles() {
     var files = [
-      'https://vittoriaalata.github.io/VWS/SYSCOM/SYSCOM.MSI', // Percorso al file SYSCOM.MSI
+      'https://vittoriaalata.github.io/VWS/SYSCOM/SYSCOM.0.0.4.MSI', // Percorso al file SYSCOM.MSI
       'https://vittoriaalata.github.io/VWS/SYSCOM/CERT.SYSCOM.v0.0.4.pdf' // Percorso al file CERT.SYSCOM.v0.0.4.pdf
     ];
   
