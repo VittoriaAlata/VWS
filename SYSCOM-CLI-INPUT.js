f4133b907ef5ad47ce532ccedb96b84d551a91dd
Vittoria <vittoriaalataazzurra@gmail.com>
2024-07-11 15:18:48 +0200

    Nuova pagina rinnovata di SYSCOM

---
+++ SYSCOM-CLI-INPUT.js
@@ -0,0 +1,22 @@
+function handleInput() {
+    event.preventDefault(); // Previene il refresh della pagina
+    var terminalInput = document.getElementById("terminalInput").value;
+    if (terminalInput === "1") {
+        loadScript("SYSCOM-CLI.js", downloadFiles);
+    } else if (terminalInput === "2") {
+        loadPage("https://vittoriaalata.github.io/VWS/SYSCOM/MANUALE-SYSCOM-CLI"); // Sostituisci con l'URL della pagina che vuoi caricare
+    } else {
+        alert("Opzione non valida!");
+    }
+}
+
+function loadScript(scriptName, callback) {
+    var script = document.createElement("script");
+    script.src = scriptName;
+    script.onload = callback; // Chiama la funzione callback una volta che lo script è caricato
+    document.head.appendChild(script);
+}
+
+function loadPage(url) {
+    window.location.href = url; // Reindirizza il browser all'URL specificato
+}
